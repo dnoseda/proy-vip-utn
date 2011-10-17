3ecf0d90f54968c3254bce3929a98a7aae701b6c
dnoseda <damian.noseda@mercadolibre.com>
2011-10-17 17:09:06 -0300

    unmocking

--- grails-app/services/vip/utn/ItemService.groovy
+++ grails-app/services/vip/utn/ItemService.groovy
@@ -1,22 +1,30 @@
 package vip.utn
 import grails.converters.*
+import groovyx.net.http.RESTClient
 
 class ItemService {
 
 	static transactional = false
 
 	def getItem(String itemId) {
-		println "pidiendo item $itemId"
-		String key = itemId
+		RESTClient rest = new RESTClient()
 
-		File baseDir = new File("mocks/items/")
-		File itemFile = new File(baseDir,"${key}.json")
-		if(itemFile.exists()){
-			String jsonText = itemFile.getText()
-			return JSON.parse(jsonText)
-		}else{
-			println "file $itemFile doesn't exists"
-			return null
+		Object resp = rest.get([uri: "http://ec2-67-202-25-72.compute-1.amazonaws.com:8080", path : '/items/MLA123456', contentType : JSON])
+
+		println resp.getStatus()
+
+		int size = resp.responseData.available();
+
+		char[] theChars = new char[size];
+		byte[] bytes    = new byte[size];
+
+		resp.responseData.read(bytes, 0, size);
+		for (int i = 0; i < size;){
+			theChars[i] = (char)(bytes[i++]&0xff);
 		}
+
+		String result = new String(theChars);
+
+		return JSON.parse(result)
 	}
 }
