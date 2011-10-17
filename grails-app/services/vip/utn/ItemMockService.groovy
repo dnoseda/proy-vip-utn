1267d31699318d64ade650bc39801b2b5f1c3fab
dnoseda <damian.noseda@mercadolibre.com>
2011-10-17 16:10:15 -0300

    with unmocked service

---
+++ grails-app/services/vip/utn/ItemMockService.groovy
@@ -0,0 +1,21 @@
+package vip.utn
+
+import grails.converters.*
+class ItemMockService {
+	static transactional = false
+
+	def getItem(String itemId) {
+		println "pidiendo item $itemId"
+		String key = itemId
+
+		File baseDir = new File("mocks/items/")
+		File itemFile = new File(baseDir,"${key}.json")
+		if(itemFile.exists()){
+			String jsonText = itemFile.getText()
+			return JSON.parse(jsonText)
+		}else{
+			println "file $itemFile doesn't exists"
+			return null
+		}
+	}
+}
---
+++ test/unit/vip/utn/ItemMockServiceTests.groovy
@@ -0,0 +1,18 @@
+package vip.utn
+
+import grails.converters.*
+import grails.test.*
+
+class ItemMockServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
